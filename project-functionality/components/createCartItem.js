15d428de588ba5d2274415bba193eee38a2a509b
shokhrukh <114140248+Mrshokhrukh@users.noreply.github.com>
2025-01-26 12:58:39 +0500

    javascript

---
+++ project-functionality/components/createCartItem.js
@@ -0,0 +1,40 @@
+import { loadCartPage } from "../pages/cart.js";
+import { removeFromCart, updateCartQuantity } from "../utils/storage.js";
+
+export function createCartItem(item) {
+  const cartItem = document.createElement("div");
+  cartItem.classList.add("cart-item");
+
+  cartItem.innerHTML = `
+        <img src="${item.image}" alt="${item.name}" class="cart-item-img">
+        <div class="cart-item-info">
+            <h4>${item.name}</h4>
+            <p>Price: $${item.price}</p>
+            <input type="number" value="${item.quantity}" min="1" class="cart-item-qty">
+
+            <button class="increase"> + </button>
+            <button class="decrease"> - </button>
+            <button class="remove-item"> Remove </button>
+        </div>
+    `;
+
+  cartItem.querySelector(".increase").addEventListener("click", (e) => {
+    updateCartQuantity(item.id, "increase");
+
+    loadCartPage();
+  });
+  
+  cartItem.querySelector(".decrease").addEventListener("click", (e) => {
+    updateCartQuantity(item.id, "decrease");
+
+    loadCartPage();
+  });
+
+  cartItem.querySelector(".remove-item").addEventListener("click", () => {
+    removeFromCart(item.id);
+
+    loadCartPage();
+  });
+
+  return cartItem;
+}
---
+++ script.js
@@ -0,0 +1,9 @@
+// import { loadCartPage } from "./project-functionality/pages/cart.js";
+// import { renderProducts } from "./project-functionality/pages/productsRender.js";
+import { loadPage } from "./project-functionality/routes/routes.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  loadPage(location.pathname);
+  //   renderProducts();
+  //   loadCartPage();
+});
