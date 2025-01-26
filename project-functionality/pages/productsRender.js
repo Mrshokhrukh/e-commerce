ae61e20e848337116d905efdf1e9eaf0330023e1
azizbekhafizov <azizbekhafizov678@gmail.com>
2025-01-26 17:28:46 +0500

    new

--- project-functionality/pages/productsRender.js
+++ project-functionality/pages/productsRender.js
@@ -2,27 +2,22 @@ import { createProductCard } from "../components/createProductCard.js";
 import { fetchProducts } from "../services/api.js";
 
 export const renderProducts = async function () {
-  const productContainer = document.getElementById("products-container");
+  const productContainer = document.querySelector(".products-container");
+  console.log(productContainer);
 
   try {
-    const products = await fetchProducts();
+    const { products } = await fetchProducts();
 
     if (products.length === 0) {
       productContainer.innerHTML = `<p>No products available.</p>`;
       return;
     }
 
-    const productGrid = document.createElement("div");
-    productGrid.classList.add("product-grid");
-
-    products.forEach((product) => {
+    products.slice(0, 10).forEach((product) => {
       const productCard = createProductCard(product);
-      productGrid.appendChild(productCard);
+      productContainer.appendChild(productCard);
     });
-
-    productContainer.appendChild(productGrid);
-    
   } catch (error) {
-    productContainer?.innerHTML = `<p>Failed to load products. Please try again later.</p>`;
+    productContainer.innerHTML = `<p>Failed to load products. Please try again later.</p>`;
   }
 };
