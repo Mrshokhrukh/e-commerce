b1368665e3d530cc5e8fb5f911fcd4abfd0d8667
shokhrukh <114140248+Mrshokhrukh@users.noreply.github.com>
2025-01-27 14:08:04 +0500

    change

--- project-functionality/pages/productsRender.js
+++ project-functionality/pages/productsRender.js
@@ -3,11 +3,10 @@ import { fetchProducts } from "../services/api.js";
 
 export const renderProducts = async function () {
   const productContainer = document.querySelector(".products-container");
-  console.log(productContainer);
 
   try {
     const { products } = await fetchProducts();
-
+    
     if (products.length === 0) {
       productContainer.innerHTML = `<p>No products available.</p>`;
       return;
